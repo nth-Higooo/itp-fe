bfe1dd14f537dcaac47c4f8530f482c3473891a7
nth-Higooo <nth.hiennguyenthanh@gmail.com>
2025-05-17 01:21:05 +0700

    update

--- src/config-global.ts
+++ src/config-global.ts
@@ -5,7 +5,7 @@ import packageJson from '../package.json';
 // ----------------------------------------------------------------------
 
 export const CONFIG = {
-  appName: 'ERP - Wata software',
+  appName: 'HRMS - ITP',
   appVersion: packageJson.version,
   serverUrl: import.meta.env.VITE_SERVER_URL ?? '',
   assetsDir: import.meta.env.VITE_ASSETS_DIR ?? '',
